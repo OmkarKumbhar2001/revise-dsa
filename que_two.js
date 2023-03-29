300cc11f86c1eedf45034d31f22aaeb557ce0fb6
Omkar Kumbhar <83999016+OmkarKumbhar2001@users.noreply.github.com>
2023-03-29 16:34:19 +0530

    Update que_two.js

--- que_two.js
+++ que_two.js
@@ -1,12 +1,22 @@
-let str =" I Love India ";
-// console.log(str)
-function remove_spaces(index_num,str){
-    let a=str.replace(" ","")
-    a=a.slice(0,-1)+""
-    return a
-}
-let first_str=remove_spaces(1,str)
-first_str=remove_spaces(-1,str);
+let str = "     I Love India    ";
+console.log(str.length)
+function remove_spaces(str) {
+  // Remove leading spaces
+  if (str.charAt(0) === " ") {
+    str = str.substring(1);
+    return remove_spaces(str);
+  }
 
-console.log(first_str)
-console.log(first_str.length)
+  // Remove trailing spaces
+  if (str.charAt(str.length - 1) === " ") {
+    str = str.substring(0, str.length - 1);
+    return remove_spaces(str);
+  }
+
+  return str;
+}
+
+let first_str = remove_spaces(str);
+
+console.log(first_str);
+console.log(first_str.length);
